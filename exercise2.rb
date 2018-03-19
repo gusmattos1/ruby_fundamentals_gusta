74bf53e470468e0f1c3869fa452f154e41b54105
gusmattos1 <gusmattos@gmail.com>
2018-03-19 14:53:06 -0400

    exercise 2.1 adjusted

--- exercise2.rb
+++ exercise2.rb
@@ -1,30 +1,28 @@
-#exercise 2
-
+#exercise 2.1
+#title
 puts "\n\n\nTips calculator \n\n\n"
-
+#asking the amout of the bill
 puts "Please inform the total of the bill"
 bill=gets.chomp.to_i
-
+#asking the % of tips to give
 puts "\n\nhow many % do you want to give as tips?"
 tips=gets.chomp.to_i
-
-while tips < 5
-  puts "don´t be so stingy"
-  tips=gets.chomp.to_i
-end
-
+#comment about the amout of tips
 while tips < 10
-  puts "you can do better"
+  if tips < 5
+    puts "don´t be so stingy"
+  elsif tips < 10
+    puts "you can do better"
+  end
   tips=gets.chomp.to_i
 end
 
-if tips > 20
-  puts "you are really generous"
-else
-  puts "thank you"
+if  tips > 20
+    puts "you are really generous"
+  else
+    puts "thank you"
 end
-
-
+#generate total bill + tips
 total_tips=bill*tips/100.to_i
 
 puts "\n\nThe total of tips will be:"
@@ -33,7 +31,5 @@ puts "#{total_tips}"
 puts "\n\nthe total (bill + tips) will be:"
 puts "#{total_tips+bill}\n\n\n"
 
-
-
-
-  # puts "#{bill*tips/100}"
+# ========================================================================================================= exercise 2.2 =========================================================================================================
+# exercise 2.2
