b373ec06bb44778be592c8b8983edebb46918071
gusmattos1 <gusmattos@gmail.com>
2018-03-19 13:59:48 -0400

    exercise1

---
+++ exercise1.rb
@@ -0,0 +1,24 @@
+# puts 2 + 3
+#
+# puts 2
+# puts 3
+# puts 2+3
+#
+# # puts 3 puts 2
+#
+# puts 'hello world'
+#
+# puts "hello world"
+#
+# puts "Betty's pie shop"
+# puts 'Betty\'s pie shop'
+
+# puts "Ada Lovelace lived for #{1852 - 1815} years."
+
+# Ada Lovelace lived for 37 years.
+
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
